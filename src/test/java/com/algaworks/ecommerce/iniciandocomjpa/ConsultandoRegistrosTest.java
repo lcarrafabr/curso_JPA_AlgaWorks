f6a3336e6ada69a1ea95141c7b08214d1a3fc516
lcarrafabr <lcarrafa.br@gmail.com>
2020-07-29 18:55:55 -0300

    Busacando objetos por identificador

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,9 +1,7 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -33,5 +31,28 @@ public class ConsultandoRegistrosTest {
     @After
     public void tearDown() {
         entityManager.close();
+    }
+
+    @Test
+    public void buscarPorIdentificador() {
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        //Produto produto = entityManager.getReference(Produto.class, 1);
+
+        System.out.println(produto.getNome());
+
+        Assert.assertNotNull(produto);
+        Assert.assertEquals("Kindle", produto.getNome());
+    }
+
+    @Test
+    public void atualizarAReferencia(){
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setNome("Microfone BM 800");
+        System.out.println(produto.getNome());
+        entityManager.refresh(produto);
+        System.out.println(produto.getNome());
+        Assert.assertEquals("Kindle", produto.getNome());
     }
 }
