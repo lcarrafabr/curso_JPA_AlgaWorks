5364bc85552b409dab866724bc733841b8374edf
lcarrafabr <lcarrafa.br@gmail.com>
2020-10-18 14:34:35 -0300

    Cache em primeiro nivel

---
+++ src/test/java/com/algaworks/ecommerce/conhecendoentitymanager/CachePrimeiroNivel.java
@@ -0,0 +1,25 @@
+package com.algaworks.ecommerce.conhecendoentitymanager;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.Test;
+
+public class CachePrimeiroNivel extends EntityManagerTest {
+
+    @Test
+    public void verificaCache() {
+
+        Produto produto = entityManager.find(Produto.class,1);
+
+        System.out.println(produto.getNome());
+        System.out.println("----------------------------------------------------");
+
+        //entityManager.clear();
+
+        Produto produtoResgatato = entityManager.find(Produto.class,produto.getId());
+
+        System.out.println(produtoResgatato.getNome());
+
+    }
+
+}
